76fc7f69b8dec51ed64a759ebe877bcecb67bc37
Wang Zhe <wangzhe@Zhes-MacBook-Pro.local>
2020-03-06 11:26:53 +0800

    1,convert it to a maven project;2, add Spring Boot ,JPA, Spring MVC, MySQL Drivers

---
+++ src/main/java/com/helix/helixbe/HelixBeApplication.java
@@ -0,0 +1,13 @@
+package com.helix.helixbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelixBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelixBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helix/helixbe/HelixBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helix.helixbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelixBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
